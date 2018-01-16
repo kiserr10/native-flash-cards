04c994e1d2f6c36f03cfedfa2123722d7f2f4223
Ross Kiser <kiserr10@gmail.com>
2018-01-15 19:45:03 -0700

    added few common components

---
+++ src/components/common/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class ButtonComponent extends Component{
+	render(){
+		return(
+			<View>
+				<Text>Button</Text>
+			</View>
+		);
+	}
+}
+export default ButtonComponent;
---
+++ src/components/common/CardSection.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CardSection extends Component {
+	render(){
+		return(
+			<View>
+				<Text>Card Section Piece</Text>
+			</View>
+		);
+	}
+}
+
+export default CardSection;
